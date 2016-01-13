b6b41dc91cf6d9f9334fa4d9077b7634b1f0723e
Davis Gossage <dcg13@duke.edu>
2016-01-12 20:01:38 -0500

    Initial commit.

---
+++ SwiftIntro.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+/*:
+# An Introduction to Swift
+
+
+- Sections\
+• [Constants and Variables](Constants%20and%20Variables)\
+• [Customizing Textual Representations](Customizing%20Textual%20Representations)
+- [Sequences and Collections](Sequences%20and%20Collections)\
+• [Understanding Value Semantics](Understanding%20Value%20Semantics)\
+• [Processing Sequences and Collections](Processing%20Sequences%20and%20Collections)\
+• [Slicing Collections](Slicing%20Collections)\
+• [Understanding Sequence and Collection Protocols](Understanding%20Sequence%20and%20Collection%20Protocols)
+- [License](License)
+****
+[Next](@next)
+*/
